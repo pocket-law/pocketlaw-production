0fc07dfc76c95ce96b7929a0c55bb926bc7512be
simplegr33n <gcgolda@gmail.com>
2017-02-26 00:50:09 -0800

    first commit

---
+++ app/src/main/java/org/pocketlaw/canada_evidence_act/ActivityMain.java
@@ -0,0 +1,284 @@
+package org.pocketlaw.canada_evidence_act;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+/**
+ * Created by gcgol on 01/18/2017.
+ */
+
+public class ActivityMain extends AppCompatActivity {
+
+    private ListView mListViewSections;
+    private AdapterSection mAdapterSection;
+
+    private ListView mListViewHeadings;
+    private AdapterHeading mAdapterHeading;
+
+    private ListView mListViewQuery;
+    private AdapterQuery mAdapterQuery;
+
+    private ImageView mBtnSearch;
+    private EditText mEdtSearch;
+    private TextView mTotalResults;
+
+    private LinearLayout loadCover;
+
+    private ImageView mBtnParts;
+    public static LinearLayout mParts;
+
+    private String LAST_SEARCH = "";
+
+    DbHelper dbHelper;
+
+    public static int partsVisible = 0;
+    public static int resultsVisible = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dbHelper = DbHelper.getInstance(getApplicationContext());
+
+        tryImport();
+
+        mAdapterSection = new AdapterSection(ActivityMain.this, R.layout.card_section, dbHelper.getAllSection());
+        mListViewSections = (ListView) findViewById(R.id.listview_section);
+        mListViewSections.setAdapter(mAdapterSection);
+
+        mAdapterHeading = new AdapterHeading(ActivityMain.this, R.layout.card_heading, dbHelper.getAllHeading());
+        mListViewHeadings = (ListView) findViewById(R.id.listview_heading);
+        mListViewHeadings.setAdapter(mAdapterHeading);
+
+        mListViewQuery = (ListView) findViewById(R.id.listview_query);
+
+        mBtnParts = (ImageView) findViewById(R.id.btn_parts);
+        mParts = (LinearLayout) findViewById(R.id.parts);
+
+        mBtnSearch = (ImageView) findViewById(R.id.btn_search);
+        mEdtSearch = (EditText) findViewById(R.id.edt_search);
+        mTotalResults = (TextView) findViewById(R.id.total_results);
+
+        loadCover = (LinearLayout) findViewById(R.id.load_cover);
+
+
+        // bring parts up or down
+        mBtnParts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mEdtSearch.setText("");
+                hideSoftKeyboard(ActivityMain.this);
+
+                partsHideShow();
+
+            }
+        });
+
+
+        // Search on enter press
+        mEdtSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+
+                    hideSoftKeyboard(ActivityMain.this);
+
+                    actionSearch();
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+        // Search on search button click
+        mBtnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mEdtSearch.requestFocus();
+
+                //TODO: this is not a perfect solution to returning focus
+                if ((mEdtSearch.length() != 0) && !(mEdtSearch.getText().toString().equals(LAST_SEARCH))) {
+                    hideSoftKeyboard(ActivityMain.this);
+                    actionSearch();
+                } else {
+                    InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputMethodManager.showSoftInput(mEdtSearch, 0);
+                }
+            }
+        });
+
+
+    }
+
+
+    private void actionSearch() {
+
+        String query = mEdtSearch.getText().toString();
+        mEdtSearch.clearFocus();
+
+        LAST_SEARCH = query;
+
+        if (!query.equals("")) {
+            mListViewQuery.setVisibility(View.VISIBLE);
+
+            mAdapterQuery = new AdapterQuery(ActivityMain.this, R.layout.card_query, dbHelper.getSearchResults(query));
+            mListViewQuery.setAdapter(mAdapterQuery);
+        }
+
+        if (mAdapterQuery.getCount() > 0) {
+            mTotalResults.setText("" + mAdapterQuery.getCount() + " Results");
+        } else {
+            mTotalResults.setText("No Results");
+        }
+
+        // Hide headings listview (inside mParts linearlayout) if it's up
+        mParts.setVisibility(View.GONE);
+
+        resultsVisible = 1;
+    }
+
+    public static void partsHideShow() {
+
+        if (partsVisible == 0) {
+            mParts.setVisibility(View.VISIBLE);
+            mParts.requestFocus();
+
+            partsVisible = 1;
+        } else if (partsVisible == 1) {
+            mParts.setVisibility(View.GONE);
+            partsVisible = 0;
+        }
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(
+                activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (resultsVisible == 1 || partsVisible == 1) {
+          //  mListViewSections.setVisibility(View.VISIBLE);
+
+            mParts.setVisibility(View.GONE);
+            mListViewQuery.setVisibility(View.GONE);
+
+            mEdtSearch.clearFocus();
+            mTotalResults.setText("");
+
+            LAST_SEARCH = "";
+            mEdtSearch.setText(LAST_SEARCH);
+
+            resultsVisible = 0;
+            partsVisible = 0;
+        } else {
+
+            if (!LAST_SEARCH.equals("")) {
+
+                mEdtSearch.setText(LAST_SEARCH);
+                actionSearch();
+
+            } else {
+                super.onBackPressed();
+            }
+        }
+
+    }
+
+    private void tryImport() {
+        try {
+            importDB();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // If the no sections in database, import via ActivityImport
+            if (dbHelper.getAllSection().size() > 1) {
+                Log.d("LoadSections:", "Success");
+            } else {
+                tryImport();
+            }
+        }
+    }
+
+    private void checkImportStatus() {
+
+        String destPath = getApplicationContext().getDatabasePath("c5").getPath();
+
+        // Create empty file at destination path
+        boolean test = new File(destPath).exists();
+
+        if (!test) {
+
+            Log.e("EEEEP", "doesn't exist");
+
+        } else {
+            Log.e("EEEEP", "exists");
+        }
+
+    }
+
+
+    private void importDB() throws IOException {
+
+        //Open your assets db as the input stream
+        InputStream in = getApplicationContext().getAssets().open("c5");
+
+        String destPath = getApplicationContext().getDatabasePath("c5").getPath();
+
+        // Create empty file at destination path
+        File f = new File(destPath);
+
+        //Open the empty db as the output stream
+        try {
+            OutputStream out = new FileOutputStream(new File(destPath));
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+            in.close();
+            out.close();
+
+            Log.e("DB Import", "imported");
+
+            checkImportStatus();
+
+
+        } catch (FileNotFoundException e) {
+            Log.e("DB Import", "File not foound" + e);
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/org/pocketlaw/canada_evidence_act/DbHelper.java
@@ -0,0 +1,276 @@
+package org.pocketlaw.canada_evidence_act;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gcgol on 01/18/2017.
+ */
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "DbHelper";
+
+    // Database Info
+    private static final String DATABASE_NAME = "c5";
+    private static final int DATABASE_VERSION = 1;
+
+    //Table Names
+    private static final String TABLE_EVIDENCE_ACT = "c5";
+
+    // Evidence Act Table Columns
+    private static final String _ID = "_id";
+    private static final String FULLTEXT = "fulltext";
+    private static final String TYPE = "type";
+    private static final String SECTION = "section";
+    private static final String PINPOINT = "pinpoint";
+
+    private static DbHelper mDbHelper;
+
+
+    public static synchronized DbHelper getInstance(Context context) {
+        // Use the application context, which will ensure that you
+        // don't accidentally leak an Activity's context.
+
+        if (mDbHelper == null) {
+            mDbHelper = new DbHelper(context.getApplicationContext());
+        }
+        return mDbHelper;
+    }
+
+
+    /**
+     * Constructor should be private to prevent direct instantiation.
+     * Make a call to the static method "getInstance()" instead.
+     */
+    private DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+   /*
+    Called when the database is created for the FIRST time.
+    If a database already exists on disk with the same DATABASE_NAME, this method will NOT be called.
+    */
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+
+        String CREATE_USERDETAIL_TABLE = "CREATE TABLE " + TABLE_EVIDENCE_ACT +
+                "(" +
+                _ID + " INTEGER PRIMARY KEY ," +
+                FULLTEXT + " TEXT, " +
+                TYPE + " TEXT, " +
+                SECTION + " TEXT, " +
+                PINPOINT + " TEXT" +
+                ")";
+        db.execSQL(CREATE_USERDETAIL_TABLE);
+
+
+
+    }
+
+
+    /*
+       Called when the database needs to be upgraded.
+       This method will only be called if a database already exists on disk with the same DATABASE_NAME,
+       but the DATABASE_VERSION is different than the version of the database that exists on disk.
+       */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion != newVersion) {
+            // Simplest implementation is to drop all old tables and recreate them
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_EVIDENCE_ACT);
+
+            onCreate(db);
+        }
+    }
+
+    /*
+   Insert a  user detail into database
+   */
+
+    public void insertSectionDetail(Section userData) {
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.beginTransaction();
+
+        try {
+            ContentValues values = new ContentValues();
+            values.put(FULLTEXT, userData.getFulltext());
+            values.put(TYPE, userData.getType());
+            values.put(SECTION, userData.getSection());
+            values.put(PINPOINT, userData.getPinpoint());
+
+            db.insertOrThrow(TABLE_EVIDENCE_ACT, null, values);
+            db.setTransactionSuccessful();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Log.d(TAG, "Error while trying to add post to database");
+        } finally {
+
+            db.endTransaction();
+        }
+
+
+    }
+
+   /*
+   fetch all Sections from database
+    */
+
+    public List<Section> getAllSection() {
+
+        List<Section> sectionDetail = new ArrayList<>();
+
+        String USER_DETAIL_SELECT_QUERY = "SELECT * FROM " + TABLE_EVIDENCE_ACT;
+
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(USER_DETAIL_SELECT_QUERY, null);
+
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    Section sectionData = new Section(1, -777, "dbhelper", "dbhelper", "dbhelper");
+                    sectionData.setID(Integer.valueOf(cursor.getString(cursor.getColumnIndex(_ID))));
+                    sectionData.setFulltext(cursor.getString(cursor.getColumnIndex(FULLTEXT)));
+                    sectionData.setType(Integer.valueOf(cursor.getString(cursor.getColumnIndex(TYPE))));
+                    sectionData.setSection(cursor.getString(cursor.getColumnIndex(SECTION)));
+                    sectionData.setPinpoint(cursor.getString(cursor.getColumnIndex(PINPOINT)));
+
+                    sectionDetail.add(sectionData);
+
+
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "Error while trying to get posts from database");
+        } finally {
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+        }
+
+        return sectionDetail;
+
+    }
+
+
+     /*
+   fetch all Headings from database
+    */
+
+    public List<Section> getAllHeading() {
+
+        List<Section> sectionDetail = new ArrayList<>();
+
+        // TODO: Display section 849
+        String USER_DETAIL_SELECT_QUERY = "SELECT * FROM " + TABLE_EVIDENCE_ACT + " WHERE type = '0'";
+
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(USER_DETAIL_SELECT_QUERY, null);
+
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    Section sectionData = new Section(1, -777, "dbhelper", "dbhelper", "dbhelper");
+                    sectionData.setID(Integer.valueOf(cursor.getString(cursor.getColumnIndex(_ID))));
+                    sectionData.setType(Integer.valueOf(cursor.getString(cursor.getColumnIndex(TYPE))));
+                    sectionData.setPinpoint(cursor.getString(cursor.getColumnIndex(PINPOINT)));
+                    sectionData.setSection(cursor.getString(cursor.getColumnIndex(SECTION)));
+                    sectionData.setFulltext(cursor.getString(cursor.getColumnIndex(FULLTEXT)));
+
+                    sectionDetail.add(sectionData);
+
+
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "Error while trying to get posts from database");
+        } finally {
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+        }
+
+        // Add schedule and related provisions
+        Section schedulesAdd = new Section(-1, 737, "schedule", "S", "Schedule");
+        sectionDetail.add(schedulesAdd);
+        Section relatedProvsAdd = new Section(-3, 737, "related_provs", "RP", "Related Provisions");
+        sectionDetail.add(relatedProvsAdd);
+
+
+        return sectionDetail;
+
+    }
+
+
+   /*
+   fetch search results from database
+    */
+
+    public List<Section> getSearchResults(String query) {
+
+        List<Section> sectionDetail = new ArrayList<>();
+
+        // TODO: there's gotta be a cleaner way than using both the list and arraylist...
+
+        String[] queryList = query.split(" ");
+
+        // Add words from Query List to Query Array, and pad blanks at end if less than 6 words
+        ArrayList queryArray = new ArrayList<>();
+        for (int i = 0; i < 6; i++){
+            if(i < queryList.length) {
+                queryArray.add(queryList[i]);
+            } else {
+                queryArray.add("");
+            }
+        }
+
+        String USER_DETAIL_SELECT_QUERY = "SELECT * FROM " + TABLE_EVIDENCE_ACT + " WHERE "
+                + FULLTEXT + " LIKE '%" + queryArray.get(0) +"%' AND " + FULLTEXT + " LIKE '%" + queryArray.get(1) +"%' AND "
+                + FULLTEXT + " LIKE '%" + queryArray.get(2) +"%' AND " + FULLTEXT + " LIKE '%" + queryArray.get(3) +"%' AND "
+                + FULLTEXT + " LIKE '%" + queryArray.get(4) +"%' AND " + FULLTEXT + " LIKE '%" + queryArray.get(5) +"%'";
+
+
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(USER_DETAIL_SELECT_QUERY, null);
+
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    Section sectionData = new Section(1, -777, "dbhelper", "dbhelper", "dbhelper");
+                    sectionData.setID(Integer.valueOf(cursor.getString(cursor.getColumnIndex(_ID))));
+                    sectionData.setFulltext(cursor.getString(cursor.getColumnIndex(FULLTEXT)));
+                    sectionData.setType(Integer.valueOf(cursor.getString(cursor.getColumnIndex(TYPE))));
+                    sectionData.setSection(cursor.getString(cursor.getColumnIndex(SECTION)));
+                    sectionData.setPinpoint(cursor.getString(cursor.getColumnIndex(PINPOINT)));
+
+                    sectionDetail.add(sectionData);
+
+
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "Error while trying to get posts from database");
+        } finally {
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+        }
+
+        return sectionDetail;
+
+    }
+
+
+}
